9ad60f4d4584da83daa59d04998abf46ffce106c
dcancot111 <lolismr21@gmail.com>
2024-04-01 11:10:59 +0200

    añadido de clase coche

--- concesionario/src/concesionario/vehiculo/coche/Ranchera.java
+++ concesionario/src/concesionario/vehiculo/coche/Ranchera.java
@@ -1,5 +1,22 @@
 package concesionario.vehiculo.coche;
 
-public class Ranchera {
 
+public class Ranchera extends Coche {
+
+	// Atributos
+    private double capacidadMaletero;
+
+ // Constructor
+    public Ranchera(Combustible combustible, double capacidadMaletero) {
+        super(combustible);
+        this.setCapacidadMaletero(capacidadMaletero);
+    }
+
+	public double getCapacidadMaletero() {
+		return capacidadMaletero;
+	}
+
+	public void setCapacidadMaletero(double capacidadMaletero) {
+		this.capacidadMaletero = capacidadMaletero;
+	}
 }
--- concesionario/src/concesionario/vehiculo/coche/Sedan.java
+++ concesionario/src/concesionario/vehiculo/coche/Sedan.java
@@ -1,5 +1,21 @@
 package concesionario.vehiculo.coche;
 
-public class Sedan {
 
+public class Sedan  extends Coche {
+	// Atributos
+    private int puertas;
+
+ // Constructor
+    public Sedan(Combustible combustible, int puertas) {
+		super(combustible);
+		this.puertas  = puertas;
+	}
+
+	public double getPuertas() {
+		return this.puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
 }
