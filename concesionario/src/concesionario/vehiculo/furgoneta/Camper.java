14a88167185dd4b59ebf59e278741acc45956bf4
ivanNavarro <inavrol1101@g.educaand.es>
2024-04-02 12:27:56 +0200

    Agregacion de metodos en la clase furgoneta

--- concesionario/src/concesionario/vehiculo/furgoneta/Camper.java
+++ concesionario/src/concesionario/vehiculo/furgoneta/Camper.java
@@ -1,5 +1,58 @@
 package concesionario.vehiculo.furgoneta;
 
-public class Camper {
+public class Camper extends Furgoneta{
 
+	//Atributos
+		private TipoFurgoneta tipoFurgoneta;
+		private int numeroCamas;
+		private String color;
+		private double tamaño;
+		
+	//Constructor
+	public Camper(String marca, String modelo, String matricula, String tipoFurgoneta, String color, int numeroCamas, double tamaño) {
+		super(marca, modelo, matricula, tipoFurgoneta);
+		this.numeroCamas = numeroCamas;
+		this.color = color;
+		this.tamaño = tamaño;
+	}
+
+	//Getters y Setters
+	public TipoFurgoneta getTipoFurgoneta() {
+		return tipoFurgoneta;
+	}
+
+	public void setTipoFurgoneta(TipoFurgoneta tipoFurgoneta) {
+		this.tipoFurgoneta = tipoFurgoneta;
+	}
+
+	public int getNumeroCamas() {
+		return numeroCamas;
+	}
+
+	public void setNumeroCamas(int numeroCamas) {
+		this.numeroCamas = numeroCamas;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(double tamaño) {
+		this.tamaño = tamaño;
+	}
+	
+	@Override
+	public void acelerar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- concesionario/src/concesionario/vehiculo/furgoneta/Furgoneta.java
+++ concesionario/src/concesionario/vehiculo/furgoneta/Furgoneta.java
@@ -5,7 +5,6 @@ import concesionario.vehiculo.Vehiculo;
 public class Furgoneta extends Vehiculo {
 	//Atributos
 		private TipoFurgoneta tipoFurgoneta;
-	
 		
 	//Constructor
 	public Furgoneta(String marca, String modelo, String matricula, String tipoFurgoneta ) {
@@ -29,7 +28,39 @@ public class Furgoneta extends Vehiculo {
 		
 	}
 	
+	public void cargarFurgoneta() {
+        boolean cargada = false;
+		if (!cargada) {
+            System.out.println("Cargando furgoneta");
+           
+            System.out.println("La furgoneta ha sido cargada.");
+            cargada = true;
+        } else {
+            System.out.println("La furgoneta ya está cargada.");
+        }
+    }
 	
+	public void descargarFurgoneta() {
+        boolean cargada = false;
+		if (cargada) {
+            System.out.println("Descargando furgoneta");
+            
+            System.out.println("La furgoneta ha sido descargada.");
+            cargada = false;
+        } else {
+            System.out.println("La furgoneta ya está vacía.");
+        }
+    }
+	
+	public void limpiarFurgoneta() {
+		boolean limpiar = false;
+		if(limpiar) {
+			System.out.println("Limpiando la furgoneta");
+        
+        	System.out.println("Furgoneta limpia y lista para usar");
+		} else {
+			System.out.println("La furgoneta ya esta lo vastante limpia");
+		}
+	}
 }
-
 
